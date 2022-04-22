694be57fe7ab9d863352a728bbe0d7a9ec543ed8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-04-22 12:10:11 -0400

    bump the cdp plugin and use the same selector

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -6,12 +6,14 @@ import 'cypress-cdp';
 it('counts', () => {
 	cy.visit('/');
 	cy.contains('[data-cy=count]', '0');
-	cy.hasEventListeners('#add1', { type: 'click' });
+	const incrementSelector = '[aria-label="Increase the counter by one"]';
+	const decrementSelector = '[aria-label="Decrease the counter by one"]';
+	cy.hasEventListeners(incrementSelector, { type: 'click' });
 	// cy.wait(1000);
-	cy.get('[aria-label="Increase the counter by one"]').click();
+	cy.get(incrementSelector).click();
 	cy.contains('[data-cy=count]', '1');
-	cy.get('[aria-label="Increase the counter by one"]').click();
+	cy.get(incrementSelector).click();
 	cy.contains('[data-cy=count]', '2');
-	cy.get('[aria-label="Decrease the counter by one"]').click();
+	cy.get(decrementSelector).click();
 	cy.contains('[data-cy=count]', '1');
 });
